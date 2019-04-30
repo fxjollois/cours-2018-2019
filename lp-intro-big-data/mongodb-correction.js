f3d7b8c7bb8cc3ea3e4fc503dd42736073bf9ac7
FX Jollois <fxjolloisupd@gmail.com>
2019-04-30 12:57:00 +0200

    Update mongodb-correction.js

--- lp-intro-big-data/mongodb-correction.js
+++ lp-intro-big-data/mongodb-correction.js
@@ -195,18 +195,21 @@ db.Sportifs.find(
     }
 )
 
-// 13. Quels sont les couples de sportifs (identifiant et nom et prénom de chaque) de même age ?
-
-// 14. Quelle est la moyenne d’âge des sportives qui pratiquent du basket ball ?
+// 13. Quelle est la moyenne d’âge des sportives qui pratiquent du basket ball ?
 db.Sportifs.aggregate([
     { $match: { "Sports.Jouer": "Basket ball", "Sexe": { $in: [ "f", "F" ]} }},
     { $group: { "_id": null, "AgeMoyen": { $avg: "$Age" }}}
 ])
 
-// 15. Quels sont les sportifs les plus jeunes ?
+// 14. Quels sont les sportifs les plus jeunes ?
+On stocke le résultat de l’aggrégation dans une variable.
+
 var agemin = db.Sportifs.aggregate([ 
     { $group: { _id: null, "agemin": { $min: "$Age" } } } 
 ]).next();
+
+On cherche les sportifs avec Age = agemin.agemin.
+
 db.Sportifs.find(
     {
         "Age": agemin.agemin
@@ -218,12 +221,33 @@ db.Sportifs.find(
     }
 )
 
-// 16. Quels sont les gymnases de “Stains” ou de “Montmorency” qui ont la plus grande surface ?
+// 15. Quels sont les gymnases de “Stains” ou de “Montmorency” qui ont la plus grande surface ?
+var grandStains = db.Gymnases.aggregate([
+	{ "$match": { "Ville": "STAINS" } },
+    { "$group": { "_id": "$Ville", "surfmax": { "$max": "$Surface" }} }
+]).next();
+var grandMontmorency = db.Gymnases.aggregate([
+	{ "$match": { "Ville": "MONTMORENCY" } },
+    { "$group": { "_id": "$Ville", "surfmax": { "$max": "$Surface" }} }
+]).next();
 
+db.Gymnases.find(
+	{ $or: [
+		{ Ville: "STAINS", Surface: grandStains["surfmax"] },
+		{ Ville: "MONTMORENCY", Surface: grandMontmorency["surfmax"] }
+	]},
+	{ _id: 0, NomGymnase: 1, Ville: 1, Surface: 1}
+)
 
-// 17. Quels entraîneurs n’entraînent que du hand ball ou du basket ball ?
+// 16. Quels entraîneurs n’entraînent que du hand ball ou du basket ball ?
+On récupère la liste des sports entraînés.
+
 var sports = db.Sportifs.distinct("Sports.Entrainer");
+On fait un filtre pour supprimer le hand et le basket.
+
 var autres = sports.filter(function(s) { return (s != "Hand ball" & s != "Basket ball") });
+On cherche ceux qui entraîne l’un ou l’autre, mais pas d’autres sports.
+
 db.Sportifs.find(
     {
         $and: [
@@ -241,9 +265,7 @@ db.Sportifs.find(
     }
 )
 
-// 18. Quels sont les couples de sportifs (identifiant et nom et prénom de chaque) de même âge avec le même conseiller ?
-
-// 19. Quels sportifs n’ont pas de conseillers ?
+// 17. Quels sportifs n’ont pas de conseillers ?
 db.Sportifs.find(
     {
         "IdSportifConseiller": { $exists: false }
@@ -254,14 +276,14 @@ db.Sportifs.find(
     }
 )
 
-// 20. Pour chaque sportif donner le nombre de sports qu’il arbitre
+// 18. Pour chaque sportif donner le nombre de sports qu’il arbitre
 db.Sportifs.aggregate([
     { $match: { "Sports.Arbitrer": { $exists: true }}},
     { $unwind: "$Sports.Arbitrer"},
     { $group: { _id: "$Nom", "nbArbitrer": { $sum: 1 }}}
 ])
 
-// 21. Pour chaque gymnase de Stains donner par jour d’ouverture les horaires des premières et dernières séances
+// 19. Pour chaque gymnase de Stains donner par jour d’ouverture les horaires des premières et dernières séances
 db.Gymnases.aggregate([
     { $match: { "Ville": "STAINS" } },
     { $unwind: "$Seances" },
@@ -269,8 +291,13 @@ db.Gymnases.aggregate([
     { $group: { _id: { "nom": "$nom", "jour": "$jour" }, "debut": { $min: "$h"}, "fin": { $max: "$h" }}}
 ])
 
-// 22. Pour chaque entraîneurs de hand ball quel est le nombre de séances journalières qu’il assure ?
+// 20. Pour chaque entraîneurs de hand ball quel est le nombre de séances journalières qu’il assure ?
+On récupère la liste des identifiants des entraîneurs de Hand.
+
 var entraineursHand = db.Sportifs.find({ "Sports.Entrainer" : "Hand ball" }, { _id: 0, IdSportif: 1 }).toArray().map(function(e) { return e.IdSportif });
+
+On les utilise pour faire la restriction (avec le $match) dans l’aggrégation.
+
 db.Gymnases.aggregate([
     { $unwind: "$Seances" },
     { $match: { "Seances.IdSportifEntraineur": { $in: entraineursHand }}},
@@ -278,16 +305,76 @@ db.Gymnases.aggregate([
     { $group: { _id: { "entraineur": "$ent", "jour": "$jour"}, nbSeances: { $sum: 1}}}
 ])
 
-// 23. Quels sont les gymnases ayant plus de 15 séances le mercredi ?
+// 21. Quels sont les gymnases ayant plus de 15 séances le mercredi ?
+A priori, il n’y en a aucun.
+
 db.Gymnases.aggregate([
     { $unwind: "$Seances" },
     { $match: { "Seances.Jour": { $in: [ "mercredi", "Mercredi" ] }}},
     { $group: { _id: { "nom": "$NomGymnase", "ville": "$Ville" }, "nbMercredi": { $sum: 1 }}},
-    { $match: { "nbMercredi": { $gte: 2 }}}
+    { $match: { "nbMercredi": { $gte: 15 }}}
 ])
 
-// 24. Pour chaque gymnase de Montmorency : quel est le nombre de séances journalières de chaque sport propose ?
-// A faire pour "s'amuser"
+// 22. Pour chaque gymnase de Montmorency : quel est le nombre de séances journalières de chaque sport propose ?
+A faire pour vous entraîner…
 
-// 25. Dans quels gymnases et quels jours y a t-il au moins 4 séances de volley ball dans la journée ?
-// Idem
+// 23. Dans quels gymnases et quels jours y a t-il au moins 4 séances de volley ball dans la journée ?
+A faire pour vous entraîner…
+
+// 24. Combien de séances assurent chaque entraineur ?
+db.Gymnases.aggregate([
+	{ $unwind: "$Seances" },
+	{ $group: { _id: "$Seances.IdSportifEntraineur", nb: { $sum: 1 }}},
+	{ $lookup: {
+		from: "Sportifs",
+		localField: "_id",
+		foreignField: "IdSportif",
+		as: "Entraineur"
+	}},
+	{ $project: { _id: 0, nb: 1, Nom: "$Entraineur.Nom" }}
+])
+
+ou par Sportifs
+db.Sportifs.aggregate([
+	{ $project: { "IdSportif": 1, "Nom": 1, _id: 0 }},
+	{ $lookup: {
+		from: "Gymnases",
+		localField: "IdSportif",
+		foreignField: "Seances.IdSportifEntraineur",
+		as: "Gymnases"
+	}},
+	{ $match: { "Gymnases": { "$ne": [] }} },
+	{ $unwind: "$Gymnases" },
+	{ $unwind: "$Gymnases.Seances" },
+	{ $match: { "$expr": { "$eq": [ "$IdSportif", "$Gymnases.Seances.IdSportifEntraineur" ]}}},
+	{ $group: { _id: "$Nom", nb: { $sum: 1 }}}
+])
+
+db.Gymnases.aggregate([
+	{ $unwind: "$Seances" },
+	{ $lookup: {
+		from: "Sportifs",
+		localField: "Seances.Libelle",
+		foreignField: "Sports.Jouer",
+		as: "Entraineur"
+	}}
+])
+
+
+// 25. Donner, pour chaque gymnase, la liste des entraîneurs (nom uniquement) qui y ont des séances
+db.Gymnases.aggregate([
+	{ $unwind: "$Seances" },
+	{ $lookup: {
+		from: "Sportifs",
+		localField: "Seances.IdSportifEntraineur",
+		foreignField: "IdSportif",
+		as: "Entraineur"
+	}},
+	{ $group: { 
+		_id: { "Nom": "$NomGymnase", "Ville": "$Ville" }, 
+		Entraineur: { "$addToSet": "$Entraineur.Nom" }
+	}}
+])
+
+
+
